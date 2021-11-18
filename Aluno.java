20e73adebd114d23bbda21ca52194033d52ef0e8
RogerioGNunes <rg.nunes2013@gmail.com>
2021-11-18 17:52:44 +0000

    exer_3 pronto!

--- Aluno.java
+++ Aluno.java
@@ -31,15 +31,11 @@ public Aluno(double nota1, double nota2, double nota3) {
 }
 
 
-
-
-
 public Aluno(String disciplina, double nota1, double nota2, double nota3) {
     this.disciplina = disciplina;
     this.nota1 = nota1;
     this.nota2 = nota2;
     this.nota3 = nota3;
-
 }
 
 
